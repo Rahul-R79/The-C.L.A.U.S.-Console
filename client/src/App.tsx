e487b752e00195143c91b638d5ae3a344d9705e2
Rahul-R79 <rahul.devworks@gmail.com>
2025-12-21 14:07:15 +0530

    chore: rename ParentHome.tsx to UserHome.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ParentHome from './pages/parent/ParentHome';
+import ParentHome from './pages/user/UserHome';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
