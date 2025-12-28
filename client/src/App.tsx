c80639884813f07c28d8ebe6a347fad67eb0ae8b
Rahul-R79 <rahul.devworks@gmail.com>
2025-12-28 10:35:03 +0530

    Fix critical bug: Remove duplicate AuthProvider causing race condition and state loss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserHome from './pages/user/UserHome';
 import ScannerPage from './pages/user/ScannerPage';
 import ScanResultPage from './pages/user/ScanResultPage';
-import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import ApprovedWishesPage from './pages/user/ApprovedWishesPage';
@@ -16,53 +15,51 @@ import SantaRoute from './components/SantaRoute';
 
 function App() {
 	return (
-		<AuthProvider>
-			<Router>
-				<Routes>
-					{/* User Routes */}
-					<Route path="/" element={<UserHome />} />
-					<Route path="/scanner" element={
-						<ProtectedRoute>
-							<ScannerPage />
-						</ProtectedRoute>
-					} />
-					<Route path="/result" element={
-						<ProtectedRoute>
-							<ScanResultPage />
-						</ProtectedRoute>
-					} />
-					<Route path="/wishes" element={
-						<ProtectedRoute>
-							<ApprovedWishesPage />
-						</ProtectedRoute>
-					} />
-					<Route path="/wishes/:id" element={
-						<ProtectedRoute>
-							<SingleWishPage />
-						</ProtectedRoute>
-					} />
+		<Router>
+			<Routes>
+				{/* User Routes */}
+				<Route path="/" element={<UserHome />} />
+				<Route path="/scanner" element={
+					<ProtectedRoute>
+						<ScannerPage />
+					</ProtectedRoute>
+				} />
+				<Route path="/result" element={
+					<ProtectedRoute>
+						<ScanResultPage />
+					</ProtectedRoute>
+				} />
+				<Route path="/wishes" element={
+					<ProtectedRoute>
+						<ApprovedWishesPage />
+					</ProtectedRoute>
+				} />
+				<Route path="/wishes/:id" element={
+					<ProtectedRoute>
+						<SingleWishPage />
+					</ProtectedRoute>
+				} />
 
 
-					{/* Santa Admin Routes */}
-					<Route path="/santa/login" element={<SantaLoginPage />} />
-					<Route path="/santa/dashboard" element={
-						<SantaRoute>
-							<SantaDashboard />
-						</SantaRoute>
-					} />
-					<Route path="/santa/queue" element={
-						<SantaRoute>
-							<SantaQueuePage />
-						</SantaRoute>
-					} />
-					<Route path="/santa/comms" element={
-						<SantaRoute>
-							<SantaCommsPage />
-						</SantaRoute>
-					} />
-				</Routes>
-			</Router>
-		</AuthProvider>
+				{/* Santa Admin Routes */}
+				<Route path="/santa/login" element={<SantaLoginPage />} />
+				<Route path="/santa/dashboard" element={
+					<SantaRoute>
+						<SantaDashboard />
+					</SantaRoute>
+				} />
+				<Route path="/santa/queue" element={
+					<SantaRoute>
+						<SantaQueuePage />
+					</SantaRoute>
+				} />
+				<Route path="/santa/comms" element={
+					<SantaRoute>
+						<SantaCommsPage />
+					</SantaRoute>
+				} />
+			</Routes>
+		</Router>
 	)
 }
 
